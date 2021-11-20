dfd6d4175f07fadea6f26358b7608c17bab09248
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2021-11-20 15:06:01 +0900

    コメント追加

--- src/Component/atoms/MajAndMinSwitch.js
+++ src/Component/atoms/MajAndMinSwitch.js
@@ -4,6 +4,11 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { Key } from "@tonaljs/tonal";
 
+/**
+ * メジャーとマイナーを切り替えるボタン
+ * @param {*} param0 
+ * @returns 
+ */
 const MajAndMinSwitch = ({ isMajKey, setIsMajKey, rootKey, setRootKey }) => {
 
     const _onChane = (event) => {
