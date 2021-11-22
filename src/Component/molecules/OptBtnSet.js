afa600aee99bfe993dbaf1bb02fb2206ed9acccc
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2021-11-22 10:52:18 +0900

    fixed

--- src/Component/molecules/OptBtnSet.js
+++ src/Component/molecules/OptBtnSet.js
@@ -4,9 +4,10 @@ import { Note, Chord, ChordType } from "@tonaljs/tonal";
 
 function OptBtnSet({ holdingChord, setHoldingChord }) {
 
-    const optList = ["2M", "3m", "3M", "4P", "5d", "5P", "6m", "6M", "7m", "7M", "9m", "9M", "9A", "11m", "11M", "13M"];
+    const optList = ["2M", "3m", "3M", "4P", "5d", "5P","5A", "6m", "6M", "7m", "7M", "9m", "9M", "9A", "11P", "11A", "13m", "13M"];
 
     const showLog = () => {
+        console.log(ChordType.all().map(get => get.intervals));
     }
 
     return (
