43d1c9e29a26140ec18f0fc93c4181f5a05a9dc3
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2022-02-24 17:20:09 +0900

    同上

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  // 各パラメータの名前が
+  // 各パラメータの名前がmatchersで一致した際、以下の変数として、storybook上で編集できるようになる
   controls: {
     matchers: {
       color: /(background|color)$/i,
