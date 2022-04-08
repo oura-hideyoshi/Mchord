133b7a27250078c34ba5330ce9a472ed613a7a1e
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2022-04-08 11:56:17 +0900

    初期化ボタン追加

--- src/components/organisms/SideBar.tsx
+++ src/components/organisms/SideBar.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useReactFlow } from 'react-flow-renderer'
+import { makeKeyNode } from '../libs/creator';
+import { keySignature } from '../libs/types';
 
 export const SideBar = () => {
+    const init = { sig: "#" as keySignature, isMajor: true }
+    const { setNodes } = useReactFlow();
+    const initialize = () => {
+        setNodes([
+            makeKeyNode("key_1", { x: 0, y: 0 }, { keySig: init.sig, isMajor: init.isMajor })
+        ])
+    }
     return (
         <div>
             <button onClick={() => console.clear()}>
                 clear console
+            </button>
+            <button onClick={initialize}>
+                init
             </button>
         </div>
     )
