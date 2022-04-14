ffdbae1722d7600ca7e64541e90c8016fbed9e9d
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2022-04-14 20:13:22 +0900

    新規ノードの位置

--- src/components/libs/hooks.ts
+++ src/components/libs/hooks.ts
@@ -5,17 +5,20 @@ import { keySignature, MusicalNode, MusicalNodeData, ChordNodeData } from "./typ
 import UUID from "uuidjs";
 import { useContext } from "react";
 import { MchordContext } from "../pages/Top";
+import { isKeyNode } from "./utils";
 
 export function addChordNode(instance: ReactFlowInstance<MusicalNodeData>, baseNodeId: string) {
+
     const { setNodes, getNode, getNodes, addNodes, addEdges } = instance;
     const baseNode = getNode(baseNodeId);
 
     if (baseNode == undefined)
         return
 
-    const { id, position, data: { keySig, isMajor } } = baseNode;
+    const { id, position, data: { keySig, isMajor }, type } = baseNode;
+    const newNodeSpace = isKeyNode(baseNode) ? 150 : 80;
     const newNode = makeChordNode(
-        { x: position.x + 150, y: position.y },
+        { x: position.x + newNodeSpace, y: position.y },
         {
             getChordProps: {
                 typeName: '',
