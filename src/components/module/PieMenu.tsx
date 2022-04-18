0529653f3400afc2d59d3c58f52b5ab45aedeb92
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2022-04-18 11:53:31 +0900

    pie作成

--- src/components/module/PieMenu.tsx
+++ src/components/module/PieMenu.tsx
@@ -1,12 +1,20 @@
 import { css } from '@emotion/css'
 import React, { ReactNode } from 'react'
+import { color } from '../propaties/color'
 
 interface props {
     children: ReactNode
 }
 
-const itemList = ["a", "b", "c", "d", "e"]
+const itemList = [1, 2, 3, 4, 5, 6]
 export const PieMenu = ({ }: props): JSX.Element => {
+    // skew : 図形を平行四辺形風に変形させる
+    const len = itemList.length;
+    const size = 150;
+    const oneRotateDeg = 360 / len; // 一つのピースの中心角
+    const rotateOverHeadDeg = 90 - oneRotateDeg / 2 // 角のオーバーヘッド
+    const oneSkewDeg = 90 - oneRotateDeg; // 90度からの過多角
+    // const ceterRad = 360 /
     return (
         <div
             className={css({
@@ -23,10 +31,12 @@ export const PieMenu = ({ }: props): JSX.Element => {
                     padding: 0,
                     margin: 0,
                     borderRadius: "50%",
-                    width: "calc(2 * 150px)",
-                    height: "calc(2 * 150px)"
+                    width: `calc(2 * ${size}px)`,
+                    height: `calc(2 * ${size}px)`
                 })}>
-                {itemList.map(item => {
+                {itemList.map((item, idx) => {
+                    const rotateDeg = rotateOverHeadDeg + oneRotateDeg * idx;
+                    const skewDeg = oneSkewDeg;
                     return (
                         <li
                             className={css({
@@ -35,7 +45,7 @@ export const PieMenu = ({ }: props): JSX.Element => {
                                 bottom: "50%",
                                 right: "50%",
                                 position: "absolute",
-                                transform: "rotate(30deg) skew(-30deg)",
+                                transform: `rotate(${rotateDeg}deg) skew(${oneSkewDeg}deg)`,
                                 transformOrigin: "bottom right",
                                 overflow: "hidden"
                             })}>
@@ -45,21 +55,23 @@ export const PieMenu = ({ }: props): JSX.Element => {
                                 height: "200%",
                                 transformOrigin: "50% 50%",
                                 borderRadius: "50%",
-                                transform: "skew(30deg) rotate(-30deg)",
+                                transform: `skew(${-skewDeg}deg) rotate(${-rotateOverHeadDeg}deg) `,
                                 color: "black",
                                 backgroundColor: "gray",
-                                outline: "none"
+                                outline: "none",
+                                ":hover": {
+                                    backgroundColor: color.darkgray,
+                                }
                             })}>
                                 <div className={css({
                                     position: "absolute",
                                     width: "100%",
                                     textAlign: "center",
-                                    top: "calc((50% + 150px - 50px) / 2 - 2em / 2)"
+                                    top: `calc((50% + ${size}px - 50px) / 2 - 2em / 2)`
                                 })}>
                                     <div className={css({
                                         display: "inline-block",
-                                        transform: "rotate(-0deg)",
-
+                                        transform: `rotate(${rotateOverHeadDeg - rotateDeg}deg)`,
                                     })}
                                     >
                                         {item}
